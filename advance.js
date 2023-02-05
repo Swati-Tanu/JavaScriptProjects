24506e633414608e3809869cebf50385734a679a
Swati <swatitani01@gmail.com>
2023-02-05 11:28:48 +0530

    assignment

--- advance.js
+++ advance.js
@@ -24,4 +24,13 @@ calculator(5,2,add);
 
 //Asynchronous JavaScript: 
 
+//Hoisting: It is a machenism where variables and function declaration are moved to the top of their scope before the code executed.
+
+//Scope Chain : It is used to resolve the value of variable names in JS. It is lexically defined which means we can see what the scope chain will be by looking at the code. 
+
+//Lexical Scope: It means the inner function can get access to their parent functions, variables but the vice-versa is not true.
+
+//Closures: It is a combination of a function bundled together (enclosed) with references to its surrounding state (the lexical environment). In other words, a closure gives you access to an outer function's scope from an inner function. in JS, it is created everytime a function is created, at function creation time.
+
+
 
