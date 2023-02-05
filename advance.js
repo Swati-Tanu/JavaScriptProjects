f87c727afc4c4c2b2c2db58bbf74c39f3b737007
Swati <swatitani01@gmail.com>
2023-02-05 12:33:10 +0530

    Complete

--- advance.js
+++ advance.js
@@ -32,5 +32,30 @@ calculator(5,2,add);
 
 //Closures: It is a combination of a function bundled together (enclosed) with references to its surrounding state (the lexical environment). In other words, a closure gives you access to an outer function's scope from an inner function. in JS, it is created everytime a function is created, at function creation time.
 
+//console.dir(): //It is used to describe more about the data.
 
+//Asynchornous JS vs Synchronous JS
+
+//Event Loop
+
+//Function Currying: It is technique of evaluating function with multiple arguments into sequence of function with single argument. In other words, instead of taking all arguments at one time, it takes the first one and return new function that takes second one and returns a new function and so forth, untill all arguments have been fulfilled.
+
+function sum(num1){
+    //console.log(num1);
+    return function(num2){
+        // console.log(num1,num2);
+        return function(num3){
+            console.log(num1+num2+num3);
+        }
+    }
+}
+
+sum(5)(3)(8);
+
+//Other way in one line:
+const  sum = (num1) => (num2) => (num3) => console.log(num1+num2+num3);
+
+sum(5)(3)(8);
+
+//Callback Hell, Fetch APIs, Promises/Async-Await, Error Handling in JS.
 
